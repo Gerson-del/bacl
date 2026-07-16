6aa871badfc6404f380626e1df404b3723d2ff26
Neftaly Juarez <gersonneftaly1@gmail.com>
2026-07-16 12:18:08 -0600

    cambios a bookingController

--- v2/controller/booking.controller.js
+++ v2/controller/booking.controller.js
@@ -8,28 +8,23 @@ const getCupon = async (id) => {
       throw new CustomError("El id_solicitud es requerido", 400);
     }
     const [reserva] = await executeQuery(
-      `SELECT * FROM vw_new_reservas_client WHERE id_solicitud = ? and estado <> "Cancelada"`,
+      `SELECT type, id_relacion FROM vw_details_booking WHERE id_solicitud = ? and estado <> "Cancelada"`,
       [id],
     );
-    if (!reserva) {
-      throw new CustomError("Esta reserva no existe o fue cancelada", 404);
-    }
-    if (!reserva.id_booking || reserva.id_hospedaje) {
-      const [result] = await executeSP("sp_get_solicitud_by_id", [id]);
-      return { ...result, type: "hotel" };
-    }
-    if (reserva.id_viaje_aereo) {
-      const [result, vuelos] = await Promise.all([
+
+    if (reserva.type == "flyght") {
+      const [resultQuery, vuelos] = await Promise.all([
         executeQuery(
-          `SELECT b.total, v.primer_nombre, v.segundo_nombre, v.apellido_paterno, v.apellido_materno, va.id_viaje_aereo, va.ciudad_origen as origen, va.ciudad_destino as destino, va.trip_type as tipo, va.codigo_confirmacion FROM viajes_aereos va
+          `SELECT b.total, v.primer_nombre, v.segundo_nombre, v.apellido_paterno, v.apellido_materno, va.id_viaje_aereo, va.ciudad_origen as origen, va.ciudad_destino as destino, va.trip_type as tipo, va.codigo_confirmacion 
+          FROM viajes_aereos va
           left join viajeros v on va.id_viajero = v.id_viajero
           left join bookings b on b.id_booking = va.id_booking
           WHERE va.id_viaje_aereo = ?`,
-          [reserva.id_viaje_aereo],
+          [reserva.id_relacion],
         ),
         executeQuery(
           `select eq_mano, eq_personal, eq_documentado, id_vuelo, flight_number, airline, departure_airport, departure_city, departure_date, departure_time, arrival_airport, arrival_city, arrival_date, arrival_time, stop_count as parada, seat_number, fly_type, comentarios, rate_type from vuelos where id_viaje_aereo = ?`,
-          [reserva.id_viaje_aereo],
+          [reserva.id_relacion],
         ),
       ]);
       const {
@@ -38,7 +33,7 @@ const getCupon = async (id) => {
         apellido_paterno,
         apellido_materno,
         ...rest
-      } = result[0];
+      } = resultQuery[0];
       return {
         ...rest,
         vuelos,
@@ -54,8 +49,8 @@ const getCupon = async (id) => {
           .join(" "),
       };
     }
-    if (reserva.id_renta_autos) {
-      const [result] = await executeQuery(
+    if (reserva.type == "car_rental") {
+      const [resultQuery] = await executeQuery(
         `select v.primer_nombre, v.segundo_nombre, v.apellido_paterno, v.apellido_materno, ra.nombre_proveedor, ra.codigo_renta_carro as codigo_confirmation, ra.id_conductor_principal, ra.conductor_principal, ra.conductores_adicionales, ra.descripcion_auto as tipo_auto, ra.transmission, ra.lugar_recoger_auto,
 ra.hora_recoger_auto, ra.id_sucursal_recoger_auto, ra.hora_dejar_auto, ra.lugar_dejar_auto, ra.id_sucursal_dejar_auto, ra.dias, ra.seguro_incluido, ra.additional_driver,
 b.check_in, b.check_out,
@@ -66,7 +61,7 @@ left join viajeros v on v.id_viajero = ra.id_conductor_principal
 left join sucursales sr on ra.id_sucursal_recoger_auto = sr.id_sucursal
 left join sucursales sd on ra.id_sucursal_dejar_auto = sd.id_sucursal
 left join bookings b on b.id_booking = ra.id_booking where ra.id_renta_autos = ?`,
-        [reserva.id_renta_autos],
+        [reserva.id_relacion],
       );
       const {
         primer_nombre,
@@ -74,7 +69,7 @@ left join bookings b on b.id_booking = ra.id_booking where ra.id_renta_autos = ?
         apellido_paterno,
         apellido_materno,
         ...rest
-      } = result;
+      } = resultQuery;
       return {
         ...rest,
         type: "renta_carros",
@@ -84,6 +79,48 @@ left join bookings b on b.id_booking = ra.id_booking where ra.id_renta_autos = ?
           apellido_paterno,
           apellido_materno,
         ]
+          .filter(Boolean)
+          .join(" "),
+      };
+    }
+    if (reserva.type == "hotel") {
+      const [resultQuery] = await executeQuery(
+        `
+        SELECT
+    COALESCE(vdb.check_in, s.check_in) AS check_in,
+    COALESCE(vdb.check_out, s.check_out) AS check_out,
+    COALESCE(vdb.id_confirmacion, "") AS codigo_confirmacion,
+    COALESCE(hp.comments,"") AS comentarios,
+    COALESCE(vdb.id_proveedor_service, s.id_hotel) AS id_hotel_resuelto,
+    ho.direccion AS direccion,
+    s.viajeros_adicionales AS "acompañantes",
+    COALESCE(vdb.nombre_viajero, s.nombre_viajero) AS huesped,
+    COALESCE(vdb.id_solicitud_client, s.id_solicitud) AS id_solicitud,
+    COALESCE(vdb.tipo_cuarto_vuelo, s.room) AS room,
+    COALESCE(s.is_con_desayuno, hp.is_con_desayuno) AS incluye_desayuno,
+    COALESCE(vdb.costo_total, s.total) AS total_solicitud,
+    COALESCE(vdb.created_at, s.created_at) AS created_at_solicitud,
+    'hotel' AS type
+FROM solicitudes s
+LEFT JOIN vw_details_booking vdb
+    ON vdb.id_solicitud_client = s.id_solicitud
+LEFT JOIN hospedajes hp
+    ON hp.id_hospedaje = vdb.id_relacion
+LEFT JOIN hoteles ho
+    ON hp.id_hotel = ho.id_hotel
+WHERE (
+	vdb.estado <> 'cancelada'
+    OR
+    s.status << "canceled"
+)`,
+        [reserva.id_solicitud],
+      );
+      const { check_in, check_out, codigo_confirmacion, comentarios, ...rest } =
+        resultQuery;
+      return {
+        ...rest,
+        type: "hotel",
+        viajero: [check_in, check_out, codigo_confirmacion, comentarios]
           .filter(Boolean)
           .join(" "),
       };
